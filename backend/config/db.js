446f60b3479dc7d22ca92cd7434d8a02b8ccf527
Nikhil Malviya <nikhilmalviya1245@gmail.com>
2026-06-09 10:29:19 +0530

    show mysql error

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,22 +1,19 @@
 const mysql = require("mysql2");
 
-const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "Nikhil123",   // ⚠️ most MySQL80 setups use empty password
-  database: "shopkart",
-  waitForConnections: true,
-  connectionLimit: 10
+const db = mysql.createConnection({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT || 3306
 });
 
-// TEST CONNECTION
-db.getConnection((err, connection) => {
+db.connect((err) => {
   if (err) {
     console.log("❌ MySQL NOT Connected");
-    console.log(err.message);
+    console.error(err);
   } else {
     console.log("✅ MySQL Connected Successfully");
-    connection.release();
   }
 });
 
