d9b0a17a58165f2b08ecd15b2b2ab48dadef0752
Nikhil Malviya <nikhilmalviya1245@gmail.com>
2026-06-10 11:03:32 +0530

    Update Razorpay configuration

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,13 @@ app.use("/api/users", userRoutes);
 /* =========================
    RAZORPAY INIT
 ========================= */
+
+console.log("RAZORPAY_KEY_ID =", process.env.RAZORPAY_KEY_ID);
+console.log(
+  "RAZORPAY_KEY_SECRET =",
+  process.env.RAZORPAY_KEY_SECRET ? "Loaded" : "Missing"
+);
+
 const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_KEY_SECRET
